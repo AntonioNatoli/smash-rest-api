4541a5536ac022279f836031d1b690fe67a5c021
Samuele Sciancalepore <sciancalepore.samuele@gmail.com>
2022-09-15 01:26:47 +0200

    Minor

--- smash/settings.py
+++ smash/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-^))7m(hb71ts87_)rrt8721#w@e*syrag6+9un0*ky3=%7_!((
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1'
+    '.neumann.abinsula.com '
 ]
 
 
@@ -125,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
